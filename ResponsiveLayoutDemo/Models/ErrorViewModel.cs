705d2902300590d11451433db341f9d4401e480f
jeromepeng <snowxbear@163.com>
2018-10-19 15:35:45 +0800

    Initial

---
+++ ResponsiveLayoutDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResponsiveLayoutDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
